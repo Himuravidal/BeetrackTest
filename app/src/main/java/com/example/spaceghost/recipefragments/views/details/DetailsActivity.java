52716e56d99a64d3dc52b52732a6616916f31d05
himuravidal <cristian.vidal.lopez@gmail.com>
2017-05-31 20:42:53 -0400

    ADD: Delete menu button / Remove unused resources

--- app/src/main/java/com/example/spaceghost/recipefragments/views/details/DetailsActivity.java
+++ app/src/main/java/com/example/spaceghost/recipefragments/views/details/DetailsActivity.java
@@ -6,8 +6,6 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.spaceghost.recipefragments.R;
 import com.example.spaceghost.recipefragments.adapters.PagerAdapter;
@@ -34,29 +32,6 @@ public class DetailsActivity extends AppCompatActivity {
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
         tabLayout.setupWithViewPager(viewPager);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_details, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
 
